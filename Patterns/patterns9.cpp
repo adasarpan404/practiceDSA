ad86d65f57af781df51c61ba3ae654d8792df8e7
Arpan Das <dasarpan471@gmail.com>
2022-09-25 11:47:56 +0530

    patterns 10 completed

--- Patterns/patterns9.cpp
+++ Patterns/patterns9.cpp
@@ -8,25 +8,24 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)
     {
-        for (int j = n - 1; j >= i; j--)
+        for (int j = 0; j < i; j++)
         {
             cout << " ";
         }
-        for (int j = 0; j <= i; j++)
+        for (int j = n - 1; j >= i; j--)
         {
             cout << "*";
         }
-        for (int j = 0; j < i; j++)
+        for (int j = n - 2; j >= i; j--)
         {
             cout << "*";
         }
         cout << endl;
     }
-    return 0;
 }
 
-//         *
-//        ***
-//       *****
+// 9.  *********
 //      *******
-//     *********
+//       *****
+//        ***
+//         *
